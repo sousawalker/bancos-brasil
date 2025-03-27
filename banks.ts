6e55bb03dd42793c56981c47be7cdc54c605e144
Walker Sousa <sousa.walker@gmail.com>
2025-03-27 10:14:55 -0300

    Lista completa de bancos do brasil 2025-03-27

---
+++ banks.ts
@@ -0,0 +1,618 @@
+export const banks = [
+  {
+    label: "Banco A.J.Renner S.A.",
+    value: "654"
+  },
+  {
+    label: "Banco ABN AMRO S.A.",
+    value: "075"
+  },
+  {
+    label: "Banco ABC Brasil S.A.",
+    value: "246"
+  },
+  {
+    label: "Banco Alfa S.A.",
+    value: "025"
+  },
+  {
+    label: "Banco Andbank Brasil S.A.",
+    value: "065"
+  },
+  {
+    label: "Banco B3 S.A.",
+    value: "096"
+  },
+  {
+    label: "Banco Bandepe S.A.",
+    value: "024"
+  },
+  {
+    label: "Banco Banestes S.A.",
+    value: "021"
+  },
+  {
+    label: "Banco BBM S.A.",
+    value: "107"
+  },
+  {
+    label: "Banco BCV - Banco de Crédito e Varejo S.A.",
+    value: "250"
+  },
+  {
+    label: "Banco BMG S.A.",
+    value: "318"
+  },
+  {
+    label: "Banco BNP Paribas Brasil S.A.",
+    value: "752"
+  },
+  {
+    label: "Banco BNY Mellon S.A.",
+    value: "017"
+  },
+  {
+    label: "Banco Bonsucesso S.A.",
+    value: "761"
+  },
+  {
+    label: "Banco Bradesco BBI S.A.",
+    value: "036"
+  },
+  {
+    label: "Banco Bradesco BERJ S.A.",
+    value: "122"
+  },
+  {
+    label: "Banco Bradesco Financiamentos S.A.",
+    value: "394"
+  },
+  {
+    label: "Banco Bradesco S.A.",
+    value: "237"
+  },
+  {
+    label: "Banco Bradescard S.A.",
+    value: "063"
+  },
+  {
+    label: "Banco BR Partners de Investimento S.A.",
+    value: "126"
+  },
+  {
+    label: "Banco BRB - Banco de Brasília S.A.",
+    value: "070"
+  },
+  {
+    label: "Banco BS2 S.A.",
+    value: "218"
+  },
+  {
+    label: "Banco BTG Pactual S.A.",
+    value: "208"
+  },
+  {
+    label: "Banco C6 S.A.",
+    value: "336"
+  },
+  {
+    label: "Banco Caixa Econômica Federal",
+    value: "104"
+  },
+  {
+    label: "Banco Caixa Geral - Brasil S.A.",
+    value: "473"
+  },
+  {
+    label: "Banco Capital S.A.",
+    value: "412"
+  },
+  {
+    label: "Banco Cargill S.A.",
+    value: "040"
+  },
+  {
+    label: "Banco Cédula S.A.",
+    value: "266"
+  },
+  {
+    label: "Banco Cetelem S.A.",
+    value: "739"
+  },
+  {
+    label: "Banco Citibank S.A.",
+    value: "745"
+  },
+  {
+    label: "Banco Confidence de Câmbio S.A.",
+    value: "095"
+  },
+  {
+    label: "Banco Cooperativo do Brasil S.A. - BANCOOB",
+    value: "756"
+  },
+  {
+    label: "Banco Cooperativo Sicredi S.A.",
+    value: "748"
+  },
+  {
+    label: "Banco Crédit Agricole Brasil S.A.",
+    value: "222"
+  },
+  {
+    label: "Banco Crefisa S.A.",
+    value: "069"
+  },
+  {
+    label: "Banco Cresol S.A.",
+    value: "758"
+  },
+  {
+    label: "Banco da Amazônia S.A.",
+    value: "003"
+  },
+  {
+    label: "Banco da China Brasil S.A.",
+    value: "083"
+  },
+  {
+    label: "Banco Daycoval S.A.",
+    value: "707"
+  },
+  {
+    label: "Banco de Investimentos Credit Suisse (Brasil) S.A.",
+    value: "505"
+  },
+  {
+    label: "Banco de La Provincia de Buenos Aires",
+    value: "495"
+  },
+  {
+    label: "Banco Deutsche Bank S.A.",
+    value: "487"
+  },
+  {
+    label: "Banco Digio S.A.",
+    value: "335"
+  },
+  {
+    label: "Banco do Brasil S.A.",
+    value: "001"
+  },
+  {
+    label: "Banco do Estado de Sergipe S.A.",
+    value: "047"
+  },
+  {
+    label: "Banco do Estado do Pará S.A.",
+    value: "037"
+  },
+  {
+    label: "Banco do Estado do Rio Grande do Sul S.A.",
+    value: "041"
+  },
+  {
+    label: "Banco do Nordeste do Brasil S.A.",
+    value: "004"
+  },
+  {
+    label: "Banco Fator S.A.",
+    value: "265"
+  },
+  {
+    label: "Banco Fibra S.A.",
+    value: "224"
+  },
+  {
+    label: "Banco Ficsa S.A.",
+    value: "626"
+  },
+  {
+    label: "Banco Finaxis S.A.",
+    value: "094"
+  },
+  {
+    label: "Banco Goldman Sachs do Brasil S.A.",
+    value: "064"
+  },
+  {
+    label: "Banco Guanabara S.A.",
+    value: "612"
+  },
+  {
+    label: "Banco Haitong de Investimento do Brasil S.A.",
+    value: "078"
+  },
+  {
+    label: "Banco Hana S.A.",
+    value: "755"
+  },
+  {
+    label: "Banco Hipercard S.A.",
+    value: "062"
+  },
+  {
+    label: "Banco Honda S.A.",
+    value: "370"
+  },
+  {
+    label: "Banco HSBC Brasil S.A.",
+    value: "269"
+  },
+  {
+    label: "Banco ICBC do Brasil S.A.",
+    value: "132"
+  },
+  {
+    label: "Banco Inbursa S.A.",
+    value: "012"
+  },
+  {
+    label: "Banco Indusval S.A.",
+    value: "653"
+  },
+  {
+    label: "Banco Industrial do Brasil S.A.",
+    value: "604"
+  },
+  {
+    label: "Banco Inter S.A.",
+    value: "077"
+  },
+  {
+    label: "Banco Intermedium S.A.",
+    value: "757"
+  },
+  {
+    label: "Banco Intesa Sanpaolo Brasil S.A.",
+    value: "139"
+  },
+  {
+    label: "Banco Itaú BBA S.A.",
+    value: "184"
+  },
+  {
+    label: "Banco Itaú Consignado S.A.",
+    value: "029"
+  },
+  {
+    label: "Banco Itaú S.A.",
+    value: "341"
+  },
+  {
+    label: "Banco ItauBank S.A.",
+    value: "479"
+  },
+  {
+    label: "Banco Itaú Unibanco Holding S.A.",
+    value: "652"
+  },
+  {
+    label: "Banco J. Safra S.A.",
+    value: "074"
+  },
+  {
+    label: "Banco J.P. Morgan S.A.",
+    value: "376"
+  },
+  {
+    label: "Banco John Deere S.A.",
+    value: "217"
+  },
+  {
+    label: "Banco KDB Brasil S.A.",
+    value: "076"
+  },
+  {
+    label: "Banco KEB HANA do Brasil S.A.",
+    value: "757"
+  },
+  {
+    label: "Banco KI S.A.",
+    value: "762"
+  },
+  {
+    label: "Banco Kirton Bank S.A.",
+    value: "399"
+  },
+  {
+    label: "Banco Luso Brasileiro S.A.",
+    value: "600"
+  },
+  {
+    label: "Banco Máxima S.A.",
+    value: "243"
+  },
+  {
+    label: "Banco Mercantil do Brasil S.A.",
+    value: "389"
+  },
+  {
+    label: "Banco Mercedes-Benz do Brasil S.A.",
+    value: "381"
+  },
+  {
+    label: "Banco Mizuho do Brasil S.A.",
+    value: "370"
+  },
+  {
+    label: "Banco Modal S.A.",
+    value: "746"
+  },
+  {
+    label: "Banco Morgan Stanley S.A.",
+    value: "066"
+  },
+  {
+    label: "Banco MUFG Brasil S.A.",
+    value: "456"
+  },
+  {
+    label: "Banco Nacional de Desenvolvimento Econômico e Social BNDES",
+    value: "007"
+  },
+  {
+    label: "Banco Natixis Brasil S.A.",
+    value: "014"
+  },
+  {
+    label: "Banco Next S.A.",
+    value: "735"
+  },
+  {
+    label: "Banco Novo Continental S.A.",
+    value: "753"
+  },
+  {
+    label: "Banco Omni S.A.",
+    value: "613"
+  },
+  {
+    label: "Banco Original do Agronegócio S.A.",
+    value: "079"
+  },
+  {
+    label: "Banco Original S.A.",
+    value: "212"
+  },
+  {
+    label: "Banco Ourinvest S.A.",
+    value: "712"
+  },
+  {
+    label: "Banco Pan S.A.",
+    value: "623"
+  },
+  {
+    label: "Banco Paraná S.A.",
+    value: "254"
+  },
+  {
+    label: "Banco Paulista S.A.",
+    value: "611"
+  },
+  {
+    label: "Banco Pecúnia S.A.",
+    value: "613"
+  },
+  {
+    label: "Banco PicPay S.A.",
+    value: "380"
+  },
+  {
+    label: "Banco Pine S.A.",
+    value: "643"
+  },
+  {
+    label: "Banco PJBank S.A.",
+    value: "301"
+  },
+  {
+    label: "Banco Plural S.A.",
+    value: "125"
+  },
+  {
+    label: "Banco Rabobank International Brasil S.A.",
+    value: "747"
+  },
+  {
+    label: "Banco RCI Brasil S.A.",
+    value: "356"
+  },
+  {
+    label: "Banco Rendimento S.A.",
+    value: "633"
+  },
+  {
+    label: "Banco Ribeirão Preto S.A.",
+    value: "741"
+  },
+  {
+    label: "Banco Rodobens S.A.",
+    value: "120"
+  },
+  {
+    label: "Banco Rural S.A.",
+    value: "453"
+  },
+  {
+    label: "Banco Safra S.A.",
+    value: "422"
+  },
+  {
+    label: "Banco Santander (Brasil) S.A.",
+    value: "033"
+  },
+  {
+    label: "Banco Scotiabank Brasil S.A.",
+    value: "751"
+  },
+  {
+    label: "Banco Semear S.A.",
+    value: "743"
+  },
+  {
+    label: "Banco Sistema S.A.",
+    value: "754"
+  },
+  {
+    label: "Banco Smartbank S.A.",
+    value: "630"
+  },
+  {
+    label: "Banco Société Générale Brasil S.A.",
+    value: "366"
+  },
+  {
+    label: "Banco Sofisa S.A.",
+    value: "637"
+  },
+  {
+    label: "Banco Sumitomo Mitsui Brasileiro S.A.",
+    value: "464"
+  },
+  {
+    label: "Banco Topazio S.A.",
+    value: "082"
+  },
+  {
+    label: "Banco Toyota do Brasil S.A.",
+    value: "387"
+  },
+  {
+    label: "Banco Triangulo S.A.",
+    value: "634"
+  },
+  {
+    label: "Banco Tricury S.A.",
+    value: "018"
+  },
+  {
+    label: "Banco UBS Brasil S.A.",
+    value: "015"
+  },
+  {
+    label: "Banco Volkswagen S.A.",
+    value: "393"
+  },
+  {
+    label: "Banco Votorantim S.A.",
+    value: "655"
+  },
+  {
+    label: "Banco VR S.A.",
+    value: "610"
+  },
+  {
+    label: "Banco Western Union do Brasil S.A.",
+    value: "119"
+  },
+  {
+    label: "Banco Will S.A.",
+    value: "280"
+  },
+  {
+    label: "Banco Woori Bank do Brasil S.A.",
+    value: "124"
+  },
+  {
+    label: "Banco XP S.A.",
+    value: "348"
+  },
+  {
+    label: "Bexs Corretora de Câmbio S.A.",
+    value: "253"
+  },
+  {
+    label: "BRK S.A. Crédito, Financiamento e Investimento",
+    value: "092"
+  },
+  {
+    label: "Citibank N.A.",
+    value: "477"
+  },
+  {
+    label: "Confederação Nacional das Cooperativas Centrais Unicred",
+    value: "136"
+  },
+  {
+    label: "Confederação Nacional das Cooperativas de Crédito",
+    value: "133"
+  },
+  {
+    label: "Confidence Corretora de Câmbio S.A.",
+    value: "060"
+  },
+  {
+    label: "Coop de Créd. Mútuo dos Despachantes de Trânsito de SC e Rio Grande do Sul",
+    value: "016"
+  },
+  {
+    label: "Cooperativa Central de Crédito - AILOS",
+    value: "085"
+  },
+  {
+    label: "Cooperativa Central de Crédito do Rio Grande do Sul",
+    value: "091"
+  },
+  {
+    label: "Credicoamo Crédito Rural Cooperativa",
+    value: "010"
+  },
+  {
+    label: "Credisan Cooperativa de Crédito",
+    value: "089"
+  },
+  {
+    label: "Credit Suisse Hedging-Griffo Corretora de Valores S.A.",
+    value: "011"
+  },
+  {
+    label: "ING Bank N.V.",
+    value: "492"
+  },
+  {
+    label: "JPMorgan Chase Bank",
+    value: "488"
+  },
+  {
+    label: "Mercado Pago",
+    value: "323"
+  },
+  {
+    label: "MONEYCORP Banco de Câmbio S.A.",
+    value: "259"
+  },
+  {
+    label: "Nu Pagamentos S.A. (Nubank)",
+    value: "260"
+  },
+  {
+    label: "PagSeguro Internet S.A.",
+    value: "290"
+  },
+  {
+    label: "Pólocred Sociedade de Crédito ao Microempreendedor",
+    value: "093"
+  },
+  {
+    label: "Senff S.A. Crédito, Financiamento e Investimento",
+    value: "276"
+  },
+  {
+    label: "Stone Pagamentos S.A.",
+    value: "197"
+  },
+  {
+    label: "Travelex Banco de Câmbio S.A.",
+    value: "095"
+  },
+  {
+    label: "Uniprime Norte do Paraná",
+    value: "084"
+  },
+  {
+    label: "UP.P Sociedade de Empréstimo Entre Pessoas S.A.",
+    value: "373"
+  }
+];
